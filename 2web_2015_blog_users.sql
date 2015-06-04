012560870e5e487a541ccc931060eb9a7f84f7ac
kendo5731 <aur.baumann@gmail.com>
2015-06-04 22:21:55 +0200

    Add users.sql file

---
+++ 2web_2015_blog_users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `users` (`id`, `username`, `password`)
+VALUES
+	(1,'admin','admin');
